50e0699242b03c88975cb04bda0c5ff21909446c
Dexie <dexie@solumie.system>
2026-02-22 05:20:57 -0500

    Heartbeat 2026-02-22 05:20 EST: gitignore txtfiles secrets, ray-bot DM intents enabled, append daily log & heartbeats

--- ray-bot/index.js
+++ ray-bot/index.js
@@ -3,6 +3,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [
+    GatewayIntentBits.DirectMessages,
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
